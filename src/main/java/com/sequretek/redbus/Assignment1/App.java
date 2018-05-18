e209a8bc4e8f0a6ed8847c483a1098dc93496f5d
priya <priya@LAPTOP-H5PVVSBB>
2018-05-18 11:21:53 +0530

    Mouse hover assignment

---
+++ src/main/java/com/sequretek/redbus/Assignment1/App.java
@@ -0,0 +1,13 @@
+package com.sequretek.redbus.Assignment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
